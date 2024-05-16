6cf12f0cc69c40be7b04eb4bedde531d41c9eacc
Jamie Cornick <jamie.cornick@keyin.com>
2024-05-16 09:35:16 -0230

    Added new Driver class.

--- src/main/java/com/keyin/Car.java
+++ src/main/java/com/keyin/Car.java
@@ -2,7 +2,7 @@ package com.keyin;
 
 public class Car {
     private String teamName;
-    private String driver;
+    private Driver driver;
     private String raceClass;
     private boolean active;
 
@@ -18,11 +18,11 @@ public class Car {
         this.teamName = teamName;
     }
 
-    public String getDriver() {
+    public Driver getDriver() {
         return driver;
     }
 
-    public void setDriver(String driver) {
+    public void setDriver(Driver driver) {
         this.driver = driver;
     }
 
